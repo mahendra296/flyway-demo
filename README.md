4a745f02ef0bb54043c0b1aeda1d396bdba5a53e
Dipen <monani.dip@gmail.com>
2022-12-18 17:20:41 +0530

    committed changes for flyway integration
    committed changes for flyway integration

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flyway-demo
+
+If you have add in existing database then add in properties file
+````
+spring.flyway.baselineOnMigrate=true
+````
---
+++ src/main/resources/db/migration/V1__create_test_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test (
+    id bigint unsigned NOT NULL AUTO_INCREMENT,
+    name varchar(50) NOT NULL,
+    CONSTRAINT test_primary_key PRIMARY KEY (id)
+);
